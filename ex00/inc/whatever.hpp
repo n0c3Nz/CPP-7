7dc6ba62a0b4a90852f1b509a536e1cbad4daf47
c3Nz <106092515+n0c3Nz@users.noreply.github.com>
2024-10-23 11:35:55 +0000

    Update

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -3,23 +3,21 @@
 
 #include <iostream>
 
-namespace{
-	template<typename N>
-	void swap(N &a, N &b){
-		N tmp = a;
-		a = b;
-		b = tmp;
-	}
+template<typename N>
+void swap(N &a, N &b){
+	N tmp = a;
+	a = b;
+	b = tmp;
+}
 
-	template<typename N>
-	N min(N a, N b){
-		return (a < b) ? a : b;
-	}
+template<typename N>
+N min(N a, N b){
+	return (a < b) ? a : b;
+}
 
-	template<typename N>
-	N max(N a, N b){
-		return (a > b) ? a : b;
-	}
+template<typename N>
+N max(N a, N b){
+	return (a > b) ? a : b;
 }
 
 #endif
